a5216441da55ec7181b0d2aaa5da6575ee4c2e84
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-03-26 14:31:27 -0300

    Longest Substring Without Repeating Characters

---
+++ LeetCode_Longest_Substring_Without_Repeating_Characters/Longest_Substring_Without_Repeating_Characters/Longest_Substring_Without_Repeating_Characters/Solution.cs
@@ -0,0 +1,26 @@
+﻿public class Solution {
+    public int LengthOfLongestSubstring(string s)
+    {
+        int maxLenght = 0;
+        
+        for(int i = 0; i < s.Length; i++)
+        {
+            HashSet<char> charSet = new HashSet<char>();
+            int lenght = 0;
+            for (int j = i; j < s.Length; j++)
+            {
+                if (charSet.Contains(s[j]))
+                {
+                    break;
+                }
+
+                charSet.Add(s[j]);
+                lenght++;
+            }
+
+            maxLenght = Math.Max(maxLenght, lenght);
+        }
+
+        return maxLenght;
+    }
+}
