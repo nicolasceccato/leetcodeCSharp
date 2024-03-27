36e39390ff4651c31a939edb0fcac5de567451ba
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-03-27 15:22:52 -0300

    leetcode Roman To Integer

---
+++ LeetCode_RomanToInteger/RomanToInteger/RomanToInteger/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace RomanToInteger;
+
+public class Program
+{
+    public static void Main()
+    {
+        Solution solution = new Solution();
+        Console.WriteLine(solution.RomanToInt("III"));
+        Console.WriteLine(solution.RomanToInt("LVIII"));
+        Console.WriteLine(solution.RomanToInt("MCMXCIV"));
+    }
+}
