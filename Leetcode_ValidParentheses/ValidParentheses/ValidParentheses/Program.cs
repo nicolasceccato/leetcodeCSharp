b33a662f4344b960731f5bff924f3a7755a9c48e
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-03-28 15:54:59 -0300

    leetcode valid parentheses

---
+++ Leetcode_ValidParentheses/ValidParentheses/ValidParentheses/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+public class Program
+{
+    public static void Main()
+    {
+        Solution solution = new Solution();
+        
+        Console.WriteLine(solution.IsValid("()"));
+        Console.WriteLine(solution.IsValid("()[]{}"));
+        Console.WriteLine(solution.IsValid("(]"));
+        Console.WriteLine(solution.IsValid("([){]}"));
+        Console.WriteLine(solution.IsValid("([)(){]}"));
+
+    }
+}
---
+++ Leetcode_ValidParentheses/ValidParentheses/ValidParentheses/Solution.cs
@@ -0,0 +1,10 @@
+﻿public class Solution {
+    public bool IsValid(string s) {
+        while (s.Contains("()") || s.Contains("[]") || s.Contains("{}"))
+        {
+            s = s.Replace("()", "").Replace("[]", "").Replace("{}", "");
+        }
+
+        return s.Length == 0;
+    }
+}
