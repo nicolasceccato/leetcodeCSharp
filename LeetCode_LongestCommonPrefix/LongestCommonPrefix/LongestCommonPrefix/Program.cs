1a50045525ab1607fd459a219185b2115bde8026
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-03-28 11:46:37 -0300

    leetcode Longest common prefix

---
+++ LeetCode_LongestCommonPrefix/LongestCommonPrefix/LongestCommonPrefix/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+public class Program
+    { 
+        static void Main(string[] args)
+        {
+            string[] strs = new[] { "flower", "flow", "flight" };
+            Solution solution = new Solution();
+            Console.WriteLine(solution.LongestCommonPrefix(strs));
+        }
+    }
---
+++ LeetCode_LongestCommonPrefix/LongestCommonPrefix/LongestCommonPrefix/Solution.cs
@@ -0,0 +1,29 @@
+﻿public class Solution {
+    public string LongestCommonPrefix(string[] strs) {
+        if (strs.Length == 0 || strs == null)
+        {
+            return "";
+        }
+        
+        if (strs.Length == 1)
+        {
+            return strs[0];
+        }
+
+        string prefix = strs[0];
+
+        for (int i = 1; i < strs.Length; i++)
+        {
+            while (!strs[i].StartsWith(prefix))
+            {
+                prefix = prefix.Substring(0, prefix.Length - 1);
+                if (string.IsNullOrEmpty(prefix))
+                {
+                    return "";
+                }
+            }
+        }
+
+        return prefix;
+    }
+}
