81b1b092c373f2280f67659593db9fc3cddd4d13
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-03-29 16:02:59 -0300

    leetcode merge two sorted lists

---
+++ Leetcode_MergeTwoSortedLists/MergeTwoSortedLists/MergeTwoSortedLists/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using MergeTwoSortedLists;
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next = new ListNode(4);
+
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next = new ListNode(4);
+
+        PrintListNode(solution.MergeTwoLists(l1, l2));
+
+        ListNode l3 = new ListNode();
+        ListNode l4 = new ListNode();
+
+        PrintListNode(solution.MergeTwoLists(l3, l4));
+
+        ListNode l5 = new ListNode();
+        ListNode l6 = new ListNode(0);
+
+        PrintListNode(solution.MergeTwoLists(l5, l6));
+
+    }
+
+    public static void PrintListNode(ListNode listNode)
+    {
+        if (listNode != null)
+        {
+            while (listNode.next != null)
+            {
+                Console.WriteLine(listNode.val);
+                listNode = listNode.next;
+            }
+        }
+    }
+}
---
+++ Leetcode_MergeTwoSortedLists/MergeTwoSortedLists/MergeTwoSortedLists/Solution.cs
@@ -0,0 +1,36 @@
+﻿using MergeTwoSortedLists;
+
+public class Solution {
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+        
+        ListNode dummyHead = new ListNode(0);
+        ListNode current = dummyHead;
+
+        while (list1 != null && list2 != null)
+        {
+            if (list1.val <= list2.val)
+            {
+                current.next = list1;
+                list1 = list1.next;
+            }
+            else
+            {
+                current.next = list2;
+                list2 = list2.next;
+            }
+
+            current = current.next;
+        }
+
+        if (list1 != null)
+        {
+            current.next = list1;
+        }
+        else
+        {
+            current.next = list2;
+        }
+
+        return dummyHead.next;
+    }
+}
