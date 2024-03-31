8a7af5bbd322396bee5a89ac83d40290c253fe7b
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-03-31 18:48:03 -0300

    leetcode RemoveElement

---
+++ Leetcode_RemoveElement/RemoveElement/RemoveElement/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RemoveElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] nums = new[] { 3, 2, 2, 3};
+            int val = 3;
+
+            int value = solution.RemoveElement(nums, val);
+
+            for (int i = 0; i < value; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+        }
+    }
+}
---
+++ Leetcode_RemoveElement/RemoveElement/RemoveElement/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace RemoveElement;
+
+public class Solution {
+    public int RemoveElement(int[] nums, int val)
+    {
+        int index = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+
+        return index;
+    }
+}
