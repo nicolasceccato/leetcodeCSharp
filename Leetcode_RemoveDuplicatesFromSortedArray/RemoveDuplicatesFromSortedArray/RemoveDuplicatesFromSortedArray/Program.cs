9b3037a26a74e4801257a8c461fe2358dbf928e2
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-03-31 18:33:31 -0300

    RemoveDuplicatesFromSortedArray

---
+++ Leetcode_RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RemoveDuplicatesFromSortedArray 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] nums = new[] { 1, 1, 2 };
+            int nums2 = solution.RemoveDuplicates(nums);
+
+            for (int i = 0; i < nums2; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+        }
+    }
+}
---
+++ Leetcode_RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace RemoveDuplicatesFromSortedArray;
+
+public class Solution
+{
+
+    public int RemoveDuplicates(int[] nums)
+    {
+        if (nums.Length == 0)
+        {
+            return 0;
+        }
+        
+        int index = 0;
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[index] != nums[i])
+            {
+                index++;
+                nums[index] = nums[i];
+            }
+        }
+
+        return index + 1;
+
+    }
+}
+
