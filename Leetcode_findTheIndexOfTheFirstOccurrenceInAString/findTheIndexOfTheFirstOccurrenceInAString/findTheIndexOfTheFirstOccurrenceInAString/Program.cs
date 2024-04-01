612ce1c1764eee3e33f467176a4cbfdffb81603e
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-04-01 20:56:35 -0300

    findTheIndexOfTheFirstOccurrenceInAString

---
+++ Leetcode_findTheIndexOfTheFirstOccurrenceInAString/findTheIndexOfTheFirstOccurrenceInAString/findTheIndexOfTheFirstOccurrenceInAString/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace findTheIndexOfTheFirstOccurrenceInAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string haystack = "sadbutsad";
+            string needle = "sad";
+
+            Solution solution = new Solution();
+
+            Console.WriteLine(solution.StrStr(haystack, needle));
+        }
+    }
+}
---
+++ Leetcode_findTheIndexOfTheFirstOccurrenceInAString/findTheIndexOfTheFirstOccurrenceInAString/findTheIndexOfTheFirstOccurrenceInAString/Solution.cs
@@ -0,0 +1,15 @@
+﻿namespace findTheIndexOfTheFirstOccurrenceInAString;
+
+public class Solution {
+    public int StrStr(string haystack, string needle) {
+        if (needle.Length > haystack.Length || !haystack.Contains(needle))
+        {
+            return -1;
+        }
+        
+        int index = 0;
+        index = haystack.IndexOf(needle);
+
+        return index;
+    }
+}
