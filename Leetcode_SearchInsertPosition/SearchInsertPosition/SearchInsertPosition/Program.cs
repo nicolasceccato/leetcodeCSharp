2a7f4e0fcee4fce7019f6c58d610748df27bb112
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-04-02 21:13:31 -0300

    SearchInsertPosition

---
+++ Leetcode_SearchInsertPosition/SearchInsertPosition/SearchInsertPosition/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SearchInsertPosition // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new[] { 1, 3, 5, 6 };
+            int target = 5;
+            Solution solution = new Solution();
+            Console.WriteLine(solution.SearchInsert(nums, target));
+            
+        }
+    }
+}
---
+++ Leetcode_SearchInsertPosition/SearchInsertPosition/SearchInsertPosition/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace SearchInsertPosition;
+
+public class Solution {
+    public int SearchInsert(int[] nums, int target)
+    {
+        int index = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] < target)
+            {
+                index++;
+            }
+
+            if (nums[i] == target)
+            {
+                return i;
+            }
+        }
+
+        return index;
+    }
+}
