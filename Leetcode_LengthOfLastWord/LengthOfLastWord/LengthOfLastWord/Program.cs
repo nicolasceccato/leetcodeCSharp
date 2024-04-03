7cc5fc7dfcd1236928b52f93e04e7dbdd716bf87
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-04-03 20:35:13 -0300

    Length of Last Word

---
+++ Leetcode_LengthOfLastWord/LengthOfLastWord/LengthOfLastWord/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LengthOfLastWord // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Hello World";
+            Solution solution = new Solution();
+            Console.WriteLine(solution.LengthOfLastWord(s));
+        }
+    }
+}
---
+++ Leetcode_LengthOfLastWord/LengthOfLastWord/LengthOfLastWord/Solution.cs
@@ -0,0 +1,12 @@
+﻿namespace LengthOfLastWord;
+
+public class Solution {
+    public int LengthOfLastWord(string s)
+    {
+        string[] values = s.Trim().Split(' ');
+        int size = values.Length;
+
+        string lastWord = values[size - 1];
+        return lastWord.Length;
+    }
+}
