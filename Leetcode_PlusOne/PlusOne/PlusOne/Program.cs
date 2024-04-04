772ac9673ed53b52bfbe5fd036a298b180726c46
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-04-04 18:55:37 -0300

    PlusOne

---
+++ Leetcode_PlusOne/PlusOne/PlusOne/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PlusOne // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] digits = new int[] { 1, 2, 3 };
+            Solution solution = new Solution();
+            int[] newDigits = solution.PlusOne(digits);
+            for (int i = 0; i < newDigits.Length; i++)
+            {
+                Console.WriteLine(newDigits[i]);
+            }
+        }
+    }
+}
