40ef1e3106ae0ff3443bce3a54a435aa5a1e38f4
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-04-06 20:45:26 -0300

    leetcode add binary

---
+++ Leetcode_AddBinary/AddBinary/AddBinary/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AddBinary // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "1010";
+            string b = "1011";
+            Solution solution = new Solution();
+            string c = solution.AddBinary(a, b);
+            Console.WriteLine(c);
+        }
+    }
+}
