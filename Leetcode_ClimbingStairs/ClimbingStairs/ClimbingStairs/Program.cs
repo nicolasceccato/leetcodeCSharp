a288e4375ec4b57e3eea5c86fee4d54a80dcf192
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-04-07 12:33:53 -0300

    leetcode climbing stairs

---
+++ Leetcode_ClimbingStairs/ClimbingStairs/ClimbingStairs/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ClimbingStairs // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.ClimbStairs(5));
+        }
+    }
+}
---
+++ Leetcode_ClimbingStairs/ClimbingStairs/ClimbingStairs/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace ClimbingStairs;
+
+public class Solution {
+    public int ClimbStairs(int n)
+    {
+        int one = 1;
+        int two = 1;
+
+        for (int i = n - 1; i > 0; i--)
+        {
+            int temp = one;
+            one = one + two;
+            two = temp;
+        }
+
+        return one;
+    }
+}
