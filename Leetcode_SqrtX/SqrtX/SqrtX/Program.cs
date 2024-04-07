5904a67a89e18ef1d0aad021e32d8442f14cb615
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-04-06 21:11:01 -0300

    leetcode sqrt(x)

---
+++ Leetcode_SqrtX/SqrtX/SqrtX/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SqrtX
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 4;
+            
+            Solution solution = new Solution();
+            int b = solution.MySqrt(a);
+            Console.WriteLine(b);
+        }
+    }
+}
---
+++ Leetcode_SqrtX/SqrtX/SqrtX/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace SqrtX;
+public class Solution {
+    public int MySqrt(int x) {
+        if (x == 0 || x == 1)
+        {
+            return x;
+        }
+
+        int left = 1;
+        int right = x;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            int sqrt = x / mid;
+
+            if (sqrt == mid)
+            {
+                return mid;
+            }
+            else if (sqrt < mid)
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        return right;
+    }
+}
