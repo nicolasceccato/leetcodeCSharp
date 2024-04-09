2df373668aa1283f0b591678c9483e143d15ac42
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-04-09 20:37:55 -0300

    remove duplicates from sorted list

---
+++ Leetcode_RemoveDuplicatesFromSortedList/RemoveDuplicatesFromSortedList/RemoveDuplicatesFromSortedList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RemoveDuplicatesFromSortedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            ListNode listNode = new ListNode(1);
+            listNode.next.val = 1;
+            listNode.next.next.val = 2;
+
+            ListNode result = solution.DeleteDuplicates(listNode);
+
+            while (result.next != null)
+            {
+                Console.WriteLine(result.val);
+                result = result.next;
+            }
+        }
+    }
+}
---
+++ Leetcode_RemoveDuplicatesFromSortedList/RemoveDuplicatesFromSortedList/RemoveDuplicatesFromSortedList/Solution.cs
@@ -0,0 +1,45 @@
+﻿namespace RemoveDuplicatesFromSortedList;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode DeleteDuplicates(ListNode head)
+    {
+        if (head != null)
+        {
+            ListNode current = head;
+            
+            while (current.next != null)
+            {
+                if (current.val == current.next.val )
+                {
+                    ListNode temp = current.next;
+                    current.next = temp.next;
+                }
+                else
+                {
+                    current = current.next;
+                }
+            }
+        }
+        return head;
+    }
+}
+public class ListNode {
+     public int val;
+     public ListNode next;
+     public ListNode(int val=0, ListNode next=null) 
+     { 
+         this.val = val;
+         this.next = next;
+     }
+ }
