b503da02d773934998794b5939892a9ca8f1d5c4
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-04-08 21:22:26 -0300

    Number of Students Unable to Eat Lunch

---
+++ Leetcode_NumberOfStudentsUnableToEatLunch/NumberOfStudentsUnableToEatLunch/NumberOfStudentsUnableToEatLunch/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NumberOfStudentsUnableToEatLunch // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] students = new int[] { 1, 1, 0, 0 };
+            int[] sandwiches = new int[] { 0,1,0,1 };
+
+            int studentsWithoutLunch = solution.CountStudents(students, sandwiches);
+            Console.WriteLine(studentsWithoutLunch);
+
+        }
+    }
+}
---
+++ Leetcode_NumberOfStudentsUnableToEatLunch/NumberOfStudentsUnableToEatLunch/NumberOfStudentsUnableToEatLunch/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace NumberOfStudentsUnableToEatLunch;
+
+public class Solution {
+    public int CountStudents(int[] students, int[] sandwiches)
+    {
+        Queue<int> queueOfStudents = new Queue<int>(students);
+        Queue<int> queueOfSandwiches = new Queue<int>(sandwiches);
+
+        int counter = 0;
+
+        while (queueOfStudents.Count != 0)
+        {
+            if (queueOfStudents.Peek() == queueOfSandwiches.Peek())
+            {
+                queueOfStudents.Dequeue();
+                queueOfSandwiches.Dequeue();
+                counter = 0;
+            }
+        }
+
+    }
+}
