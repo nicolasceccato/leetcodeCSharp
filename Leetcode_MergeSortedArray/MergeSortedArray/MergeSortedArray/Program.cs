0fd1b9d07a212fabe2525777a22229b84ba43fd8
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-04-10 20:20:54 -0300

    leetcode merge sorted array

---
+++ Leetcode_MergeSortedArray/MergeSortedArray/MergeSortedArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MergeSortedArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums1 = new int[] { 1, 2, 3, 0, 0, 0 };
+            int m = 3;
+            int[] nums2 = new int[] { 2, 5, 6 };
+            int n = 3;
+
+            Solution solution = new Solution();
+            solution.Merge(nums1, m, nums2, n);
+
+            for (int i = 0; i < (m + n); i++)
+            {
+                Console.WriteLine(nums1[i]);
+            }
+            
+        }
+    }
+}
---
+++ Leetcode_MergeSortedArray/MergeSortedArray/MergeSortedArray/Solution.cs
@@ -0,0 +1,32 @@
+﻿namespace MergeSortedArray;
+
+public class Solution {
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        int size = m + n;
+        
+        int i = m;
+      
+        for (int j = 0; j < n; j++, i++)
+        {
+            nums1[i] = nums2[j];
+        }
+        
+        orderArray(nums1, size);
+        
+    }
+
+    private void orderArray(int[] nums, int size)
+    {
+        for (int i = 0; i < size - 1; i++)
+        {
+            for (int j = i + 1; j < size; j++)
+            {
+                if (nums[j] < nums[i])
+                {
+                    (nums[i], nums[j]) = (nums[j], nums[i]);
+                }
+            }
+        }
+    }
+}
